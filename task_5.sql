fadbe1417668206a465648caa3332c267c5feb4a
NeverKnewNoble <nortexnoble@gmail.com>
2024-07-28 20:19:49 +0000

    task5

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,4 @@
--- Use the database (this should be passed as an argument in the mysql command)
 USE alx_book_store;
 
--- Insert a single row into the Customers table
-INSERT INTO Customers (customer_id, first_name, last_name, email, address)
-VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
