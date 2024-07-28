572ff3392115102166940a8ca235cb6e3f91ede5
NeverKnewNoble <nortexnoble@gmail.com>
2024-07-28 19:33:18 +0000

    mysqlserverV4

--- MySQLServer.py
+++ MySQLServer.py
@@ -3,18 +3,15 @@ from mysql.connector import errorcode
 
 def create_database(cursor):
     try:
-        # Check for the CREATE DATABASE statement
-        cursor.execute("CREATE DATABASE alx_book_store")
+        # CREATE DATABASE IF NOT EXISTS statement
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
     except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
-            print("Database 'alx_book_store' already exists.")
-        else:
-            print(f"Failed creating database: {err}")
+        print(f"Failed creating database: {err}")
 
 def main():
     try:
-        # Check for the code to establish a connection to the MySQL server
+        # Establishing a connection to the MySQL server
         conn = mysql.connector.connect(
             host="localhost",
             user="your_username",
@@ -23,7 +20,7 @@ def main():
         cursor = conn.cursor()
         create_database(cursor)
     except mysql.connector.Error as err:
-        # Check for the code to handle exceptions
+        # Handling exceptions
         print(f"Error: {err}")
     finally:
         if conn.is_connected():
@@ -32,5 +29,4 @@ def main():
             print("MySQL connection is closed")
 
 if __name__ == "__main__":
-    # Check for the code to handle exceptions
     main()
